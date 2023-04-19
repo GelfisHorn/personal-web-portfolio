ab44c22bc762a2b80dad99d4492066c44c083524
GelfisHorn <helphis.tech@gmail.com>
2023-04-18 22:06:38 -0300

    Hero changes
    - Removed the top right text.
    - Certificate image moved up.
    - The contact button was moved where the certificate image was.

--- src/modules/Hero/index.tsx
+++ src/modules/Hero/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import helphis from "../../assets/img/gelfis.png";
 import helphisHappy from "../../assets/img/gelfis-alegre.png";
 import certificated from "../../assets/img/certificated.jpg";
@@ -8,11 +9,12 @@ import { fadeIn } from "../../utils/motion";
 // context
 import useContextProvider from "../../hooks/useAppContext";
 // langs
-import DE from "../../langs/de/lang.json";
-import EN from "../../langs/en/lang.json";
-import ES from "../../langs/es/lang.json";
+import DE from '../../langs/de/lang.json'
+import EN from '../../langs/en/lang.json'
+import ES from '../../langs/es/lang.json'
 
 const Hero = () => {
+
   const { language } = useContextProvider();
 
   return (
@@ -25,50 +27,26 @@ const Hero = () => {
         viewport={{ once: false, amount: 0.25 }}
         className={`innerWidth ${css.container} flex flex-col gap-3`}
       >
-        <div
-          className={`flex flex-col gap-5 lg:gap-0 lg:flex-row items-center lg:items-start justify-between`}
-        >
+        {/* <a className="anchor hidden" id="iam"></a> */}
+        {/* upper elements */}
+        <div className={`flex flex-col gap-5 lg:gap-0 lg:flex-row items-center lg:items-start justify-between`}>
           <motion.span
             variants={fadeIn("right", "tween", 0.2, 1)}
             className="flex flex-col primaryText text-center lg:text-left"
           >
-            {language == "de"
-              ? DE.hero.title1
-              : language == "en"
-              ? EN.hero.title1
-              : ES.hero.title1}{" "}
-            <br />
-            {language == "de"
-              ? DE.hero.title2
-              : language == "en"
-              ? EN.hero.title2
-              : ES.hero.title2}
+            {language == 'de' ? DE.hero.title1 : language == 'en' ? EN.hero.title1 : ES.hero.title1} <br /> 
+            {language == 'de' ? DE.hero.title2 : language == 'en' ? EN.hero.title2 : ES.hero.title2}
           </motion.span>
-          <div className="flex flex-col items-center lg:items-end gap-6 lg:w-[25rem] text-center lg:text-right">
-            <motion.span
-              variants={fadeIn("left", "tween", 0.4, 1)}
-              className="secondaryText text-lg"
-            >
-              {language == "de"
-                ? DE.hero.sidetext
-                : language == "en"
-                ? EN.hero.sidetext
-                : ES.hero.sidetext}
-            </motion.span>
-          </div>
-
-          <a className="block lg:hidden " href="mailto:helphis.tech@gmail.com">
-            <div className="bg-[#0D2F3F] hover:bg-[#194F68] transition-colors text-zinc-200 w-fit px-6 py-3 rounded-lg">
-              <span className="text-lg">
-                {language == "de"
-                  ? DE.hero.button
-                  : language == "en"
-                  ? EN.hero.button
-                  : ES.hero.button}
-              </span>
-            </div>
-          </a>
+          <motion.div
+            variants={fadeIn("left", "tween", 0.5, 1)}
+            className={`${css.certificate} flex flex-col gap-3 items-center pr-7`}
+          >
+            <img className="max-w-[5rem]" src={certificated} alt="certificated" />
+            <span>FULL STACK MERN</span>
+            <span>WEB DEVELOPER</span>
+          </motion.div>
         </div>
+
 
         {/* person */}
         <motion.div
@@ -89,59 +67,28 @@ const Hero = () => {
           />
         </motion.div>
 
+        {/* email */}
+        {/* <a className={css.email} href="mailto:Helphis.tech@gmail.com">
+          helphis.tech@gmail.com
+        </a> */}
+
         {/* lower elements */}
-        <a
-          className="hidden lg:block absolute top-1/2 -translate-y-1/2"
-          href="mailto:helphis.tech@gmail.com"
-        >
-          <div className="bg-[#0D2F3F] hover:bg-[#194F68] transition-colors text-zinc-200 w-fit px-6 py-2 rounded-lg">
-            <span className="text-lg">
-              {language == "de"
-                ? DE.hero.button
-                : language == "en"
-                ? EN.hero.button
-                : ES.hero.button}
-            </span>
-          </div>
-        </a>
         <div className={css.lowerElements}>
           <motion.div
             variants={fadeIn("right", "tween", 0.3, 1)}
             className={css.experience}
           >
-            <div className="primaryText">
-              {language == "de"
-                ? DE.hero.years
-                : language == "en"
-                ? EN.hero.years
-                : ES.hero.years}
-            </div>
+            <div className="primaryText">{language == 'de' ? DE.hero.years : language == 'en' ? EN.hero.years : ES.hero.years}</div>
             <div className="secondaryText">
-              <div>
-                {language == "de"
-                  ? DE.hero.yearstext1
-                  : language == "en"
-                  ? EN.hero.yearstext1
-                  : ES.hero.yearstext1}
-              </div>
-              <div>
-                {language == "de"
-                  ? DE.hero.yearstext2
-                  : language == "en"
-                  ? EN.hero.yearstext2
-                  : ES.hero.yearstext2}
-              </div>
+              <div>{language == 'de' ? DE.hero.yearstext1 : language == 'en' ? EN.hero.yearstext1 : ES.hero.yearstext1}</div>
+              <div>{language == 'de' ? DE.hero.yearstext2 : language == 'en' ? EN.hero.yearstext2 : ES.hero.yearstext2}</div>
             </div>
           </motion.div>
-
-          <motion.div
-            variants={fadeIn("left", "tween", 0.5, 1)}
-            className={css.certificate}
-          >
-            <img src={certificated} alt="certificated" />
-            <span>FULL STACK MERN</span>
-            <span>WEB DEVELOPER</span>
-          </motion.div>
+          <a className="hidden lg:block w-fit h-fit" href="mailto:helphis.tech@gmail.com">
+            <div className="bg-[#0D2F3F] hover:bg-[#194F68] transition-colors text-zinc-200 w-fit px-6 py-2 rounded-lg">
+              <span className="text-lg">{language == 'de' ? DE.hero.button : language == 'en' ? EN.hero.button : ES.hero.button}</span>
+            </div>
+          </a>
         </div>
       </motion.div>
     </section>
